c73ae3c1aa77f580a7b81defa0c145628df6c70c
Keranda Terbang <tonthowi@gmail.com>
2020-01-24 17:07:44 +0700

    toLocaleString

--- diskon.js
+++ diskon.js
@@ -5,39 +5,18 @@
 document.getElementById("submitBtn").addEventListener("click", function(){
     var harga = document.getElementById("harga").value;
     var diskon = document.getElementById("diskon").value;
+    hargaDiskon(harga, diskon); 
 
     function hargaDiskon(hargaAwal, diskon) {
-        var potonganHarga =( hargaAwal * diskon) /100;
-        var hargaAkhir = hargaAwal - potonganHarga;
-        document.getElementById('hasil').innerHTML ='Dengan harga awal sebesar ' + hargaAwal + ' dan diskon sebesar ' + diskon + '% .Maka anda mendapatkan potongan harga sebesar ' + potonganHarga + '. Dan anda hanya membayar sebesar ' + hargaAkhir;
-    }
-
-    hargaDiskon(harga, diskon); 
+        var potonganHarga = Math.floor(( hargaAwal * diskon) /100);
+        var hargaAkhir = Math.floor(hargaAwal - potonganHarga);
+        document.getElementById("harga-awal").innerHTML = hargaAwal;
+        document.getElementById("jumlah-diskon").innerHTML = diskon.toLocaleString();
+        document.getElementById("potongan-harga").innerHTML = potonganHarga.toLocaleString();
+        document.getElementById("harga-akhir").innerHTML = hargaAkhir.toLocaleString();
+    };
+ 
 });
 
-
-
-// Discount Counter dengan Object dan Methods
-// document.addEventListener('DOMContentLoaded', function() {
- 
-//     var hitungDiskon = {
-//         hargaAwal:document.getElementsByClassName('inputForm'),
-//         diskon: document.getElementsByClassName('inputForm'),
-//         calcPotonganHarga : function() {
-//             this.potonganHarga = (this.hargaAwal * this.diskon) / 100;
-//             return this.potonganHarga;
-//         },
-//         calcHargaAkhir: function () {
-//             this.hargaAkhir = this.hargaAwal - this.potonganHarga;
-//             return this.hargaAkhir;
-    
-//         }
-//     }
-    
-//     hitungDiskon.calcPotonganHarga();
-//     hitungDiskon.calcHargaAkhir();
-    
-  
-// });
 
 
