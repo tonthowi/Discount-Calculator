961bfe1ec8f0559675f1ea84bc8598883a366843
Keranda Terbang <tonthowi@gmail.com>
2020-01-25 22:37:59 +0700

    Fix toLocaleString

--- diskon.js
+++ diskon.js
@@ -1,22 +1,24 @@
 // Discount Counter dengan function
 
-
-
 document.getElementById("submitBtn").addEventListener("click", function(){
     var harga = document.getElementById("harga").value;
     var diskon = document.getElementById("diskon").value;
-    hargaDiskon(harga, diskon); 
-
-    function hargaDiskon(hargaAwal, diskon) {
-        var potonganHarga = Math.floor(( hargaAwal * diskon) /100);
-        var hargaAkhir = Math.floor(hargaAwal - potonganHarga);
-        document.getElementById("harga-awal").innerHTML = hargaAwal;
-        document.getElementById("jumlah-diskon").innerHTML = diskon.toLocaleString();
-        document.getElementById("potongan-harga").innerHTML = potonganHarga.toLocaleString();
-        document.getElementById("harga-akhir").innerHTML = hargaAkhir.toLocaleString();
-    };
+    hargaDiskon(harga, diskon);
  
 });
+
+function hargaDiskon(harga, diskon) {
+    var potonganHarga = Math.floor(( harga * diskon) /100);
+    var hargaAkhir = Math.floor(harga - potonganHarga);
+    var harga = hargaAkhir + potonganHarga;
+
+    document.getElementById("harga-awal").innerHTML = 'IDR '+ harga.toLocaleString();
+    document.getElementById("jumlah-diskon").innerHTML = diskon.toLocaleString() +' %';
+    document.getElementById("potongan-harga").innerHTML = 'IDR '+ potonganHarga.toLocaleString();
+    document.getElementById("harga-akhir").innerHTML = 'IDR '+ hargaAkhir.toLocaleString();
+};
+
+
 
 
 
